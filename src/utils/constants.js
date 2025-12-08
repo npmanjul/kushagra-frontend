0721ee5c9a58414625c03370362b72aa7f1d4e85
Anjul Singh <anjulsingh462@gmail.com>
2025-12-09 04:06:54 +0530

    fix the url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-const API_BASE_URL = "http://localhost:8000/api/v1";
+// const API_BASE_URL = "http://localhost:8000/api/v1";
+const API_BASE_URL="https://samriddhi-storage-frontend-11ts.vercel.app/api/v1"
 export default API_BASE_URL;
