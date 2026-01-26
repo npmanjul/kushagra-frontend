3ecddff9a79a844b3d5673d8fc524e4381d5b102
Anjul Singh <anjulsingh462@gmail.com>
2026-01-26 15:31:07 +0530

    ok

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
-const API_BASE_URL = "http://localhost:8000/api/v1"
+const API_BASE_URL = 
+  process.env.NEXT_PUBLIC_ENV === "production"
+  ? "http://backend:8000/api/v1"
+  : "http://localhost:8000/api/v1";
 export default API_BASE_URL;
