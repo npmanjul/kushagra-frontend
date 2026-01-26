8ebffec9d9eec435da90b57c25f6a2385da5d3ac
Anjul Singh <anjulsingh462@gmail.com>
2026-01-26 16:23:58 +0530

    update home page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <div> Grain storage bank</div>
+      <div> Grain storage bank and this is the page </div>
     </>
   );
 };
