9fa7d111d61a062395add4ef4929b69b2453ed18
Anjul Singh <anjulsingh462@gmail.com>
2026-01-26 13:07:08 +0530

    ok

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,2 @@
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_ENV === "development"
-    ? "http://localhost:8000/api/v1"
-    : "https://samriddhi-storage-backend.vercel.app/api/v1";
+const API_BASE_URL = "http://localhost:8000/api/v1"
 export default API_BASE_URL;
